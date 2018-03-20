93f2e9421d6cc644d69909204491f0251d072798
Romero Dias <romero.dias@omnilink.com.br>
2018-03-20 17:43:56 -0300

    Add Try Catch Block

--- sendemail.py
+++ sendemail.py
@@ -1,21 +1,24 @@
 import smtplib
 
 class SendMail:
-         smtp_server=''
-         smtp_port=25
-         smtp_password=''
-         smtp_from=''
-         smtp_use_auth=True
+	smtp_server=''
+	smtp_port=25
+	smtp_password=''
+	smtp_from=''
+	smtp_use_auth=True
 
-         def send_mail(self, toaddr, subject, msg):
-                 server = smtplib.SMTP(self.smtp_server,self.smtp_port)
-
-                 if(self.smtp_use_auth == True):
-                         server.ehlo()
-                         server.starttls()
-                         server.ehlo()
-                         server.login(self.smtp_from, self.smtp_password)
-
-                 message = "Subject: " + subject + "\n\n" + msg
-                 server.sendmail(self.smtp_from, toaddr, message)
-                 server.close()
+	def send_mail(self, toaddr, subject, msg):
+		server = smtplib.SMTP(self.smtp_server,self.smtp_port)
+		try:
+			if(self.smtp_use_auth == True):
+				server.ehlo()
+				server.starttls()
+				server.ehlo()
+				server.login(self.smtp_from, self.smtp_password)
+				message = "Subject: " + subject + "\n\n" + msg
+				server.sendmail(self.smtp_from, toaddr, message)
+				server.close()
+		except Exception as inst:
+			print(type(inst))     
+			print(inst.args)     
+			print(inst)
