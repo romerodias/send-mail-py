03c32dfadeea815628a56788bd68b6fc482828d0
Romero Dias <romero.dias@omnilink.com.br>
2018-03-20 17:28:00 -0300

    Create Class SendMail

---
+++ sendemail.py
@@ -0,0 +1,21 @@
+import smtplib
+
+class SendMail:
+         smtp_server=''
+         smtp_port=25
+         smtp_password=''
+         smtp_from=''
+         smtp_use_auth=True
+
+         def send_mail(self, toaddr, subject, msg):
+                 server = smtplib.SMTP(self.smtp_server,self.smtp_port)
+
+                 if(self.smtp_use_auth == True):
+                         server.ehlo()
+                         server.starttls()
+                         server.ehlo()
+                         server.login(self.smtp_from, self.smtp_password)
+
+                 message = "Subject: " + subject + "\n\n" + msg
+                 server.sendmail(self.smtp_from, toaddr, message)
+                 server.close()
